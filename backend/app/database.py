9de492901becb7e99a85c38b8b16f9dc459f2aa5
Annapureddy Tejaswini <annapureddytejaswini7@gmail.com>
2026-06-07 09:52:45 +0530

    Fix Neon database SSL connection

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,3 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
@@ -10,24 +7,18 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300,
+    connect_args={"sslmode": "require"}
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-engine = create_engine(DATABASE_URL)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
     db = SessionLocal()
